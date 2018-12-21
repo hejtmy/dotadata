e509eb81e61f3dab924689ed06dff4c87ba7f53e
hejtmy <hejtmy@gmail.com>
2018-12-21 20:10:33 +0100

    Adds match loading data

--- loader.py
+++ loader.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-def load_matches(api, player_id, limit=100):
+def load_history(api, player_id, limit=100):
+
 	#only first 100
 	hist = api.get_match_history(account_id=player_id)
 
-	pd_matches = pd.DataFrame.from_dict(hist['matches'])
+	pd_history = pd.DataFrame.from_dict(hist['matches'])
 
-	pd_raw_players = pd_matches.players.apply(pd.Series)
+	pd_raw_players = pd_history.players.apply(pd.Series)
 	pd_raw_players = pd_raw_players.rename(columns = lambda x : 'player_' + str(x))
 
 	for i_player in range(0,10):
@@ -20,5 +21,15 @@ def load_matches(api, player_id, limit=100):
 			pd_players = pd_player
 		else: 
 			pd_players = pd.concat([pd_players, pd_player], axis=1)
-	pd_matches = pd.concat([pd_matches, pd_players], axis=1)
+			
+	pd_history = pd.concat([pd_history, pd_players], axis=1)
+	pd_history = pd_history.drop('players', axis=1)
+	return(pd_history)
+
+def fill_matches_data(api, pd_history):
+	matches = []
+	for match_id in pd_history.match_id:
+		matches.append(api.get_match_details(match_id))
+	pd_matches = pd.DataFrame.from_records(matches)
 	return(pd_matches)
+	
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import dota2api
 import loader
+import pandas as pd
+%load_ext autoreload
+%autoreload 2
 
 api = dota2api.Initialise()
 #might consider raw_mode
-pd_matches = loader.load_matches(api, 126212866)
+player_id = 126212866
+pd_history = loader.load_history(api, player_id)
+pd_matches = loader.fill_matches_data(api, pd_history)
+
+pd_matches.to_csv('matches.csv', sep=";", index=False)
